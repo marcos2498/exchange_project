cdacef3091cd0d7069b524d24b4fe94ea77c0a11
Marcos Rivera <marcosrivera2495@gmail.com>
2024-07-21 15:23:30 -0400

    added frontend

--- Mysite/urls.py
+++ Mysite/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from Mysite.views import hello_world
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', hello_world),
+    path('', views.index, name='index')
 ]
