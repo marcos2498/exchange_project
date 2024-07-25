1473865db2d75c57839abf049ff1e6d0f6c25100
Marcos Rivera <marcosrivera2495@gmail.com>
2024-07-25 19:24:36 -0400

    comma mi

--- Mysite/settings.py
+++ Mysite/settings.py
@@ -68,10 +68,10 @@ WSGI_APPLICATION = 'Mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600)
-    }
+    'default': dj_database_url.parse(
+        os.environ.get('DATABASE_URL'), conn_max_age=600
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
