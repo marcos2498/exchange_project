650822ea61e6b8ef0af6fe818830dd1222a83f92
Marcos Rivera <marcosrivera2495@gmail.com>
2024-07-27 12:05:22 -0400

    fixed extra slash

--- data/urls.py
+++ data/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'), 
     path('api/add_commodity/', views.add_commodity, name='add_commodity'),
     path('api/get_list/', views.get_list, name='get_list'),
-    path('api//remove_commodity', views.remove_commodity, name='remove_commodity')
+    path('api/remove_commodity', views.remove_commodity, name='remove_commodity')
 ]
