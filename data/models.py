3ee7983140b8f98059d2b881c394aba187f6a10b
Marcos Rivera <marcosrivera2495@gmail.com>
2024-07-28 16:05:48 -0400

    fdsd

--- data/models.py
+++ data/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Commodity(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     unit = models.CharField(max_length=60)
